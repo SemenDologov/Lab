15a066fc3d4f9528bec51ffaa514068aeb807fb4
Bikbulatov Danil <donil123123@gmail.com>
2022-04-01 20:29:30 +0500

    Bikbulatov: added date to infoListener

--- src/main/java/Laba2/infoListener.java
+++ src/main/java/Laba2/infoListener.java
@@ -9,7 +9,7 @@ public class infoListener implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 			
 		//studInfo.studCrt();
-		new studInfo("<html>Номера зачетных книжек: <br>Долгов Семен - 20130847 (26.03.2022)<br>Бикбулатов Данил - 20130717<br>Трофимов Данил - 20130841<br>Харисов Рафаэль - 20210066(26.03.2022)</html>");
+		new studInfo("<html>Номера зачетных книжек: <br>Долгов Семен - 20130847 (26.03.2022)<br>Бикбулатов Данил - 20130717 (01.04.22)<br>Трофимов Данил - 20130841<br>Харисов Рафаэль - 20210066(26.03.2022)</html>");
 	}
 	
 	
